c1a40865b9e05767320bd8e97415adb10a038fdb
Alena19971993 <alena.gar@mail.ru>
2018-03-26 21:31:05 +0300

    added some changes
    added some changes

--- Strogach/Context/GenECommands.cs
+++ Strogach/Context/GenECommands.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Strogach.Network;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace Strogach.Context
 {
-    class GenECommands
+    class GenECommands : StrogachChannel
     {
+        private Exchanger _exchanger;
+       GenECommands()
+        {
+            _exchanger = new Exchanger(this);
+        }
+
+        public void GenECommanddsAuto() { }
     }
 }
