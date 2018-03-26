eca138d5bf6e213119d997e7ff68e92b3af31498
Михаил Тарасенко <tarasenkobetor@gmail.com>
2018-03-27 00:05:34 +0300

    StrogachAddedUnity

---
+++ StrogachUnity/Assets/Code/ScriptsMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ScriptsMove : MonoBehaviour
+{
+    //Logic to remove this
+
+    public GameObject _plane;
+    public GameObject _wood;
+
+    private float i = 0;
+
+    private Vector3 startVector;
+	// Use this for initialization
+	void Start ()
+    {
+        startVector = _plane.transform.position;
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        _plane.transform.position = new Vector3(startVector.x, startVector.y - i, startVector.z + i);
+
+        Mesh mesh = new Mesh();
+
+
+
+        i += 1f;
+    }
+}
