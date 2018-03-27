232107a61a64f2dfd8d49ae36dc20f0a0e4dfd9b
Владислав Пинчуков <hatefulpain@mail.ru>
2018-03-27 23:30:37 +0300

    HueHueHue
    Испралены недочёты

---
+++ OhMyWoodWorkerSimulator/Models/Block.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OhMyWoodWorkerSimulator.Models
+{
+    class Block
+    {
+        private float _width = 0;
+        private float _length = 0;
+        public Block(float kWidth,float kLenth)
+        {
+            Width = kWidth;
+            Length = kLenth;
+        }
+
+        /// <summary>
+        /// Перегрузка оператора ToString()
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Width.ToString() + " " + Length.ToString();
+        }
+
+        /// <summary>
+        /// Щирина бруска
+        /// </summary>
+        public float Width { get => _width; set => _width = value; }
+        /// <summary>
+        /// Длина бруска
+        /// </summary>
+        public float Length { get => _length; set => _length = value; }
+    }
+}
